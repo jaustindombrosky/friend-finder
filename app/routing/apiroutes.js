1f44e2354704243410df4a4f208eb39cb41a4fb2
jaustindombrosky <austindombrosky@gmail.com>
2018-05-06 13:22:39 -0600

    npm init

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -5,3 +5,17 @@ var apiRoutes = require('./app/routing/api-routes.js');
 var htmlRoutes = require('./app/routing/html-routes.js');
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// Server Routes
+apiRoutes(app); 
+htmlRoutes(app);
+
+// LISTENER - this starts the server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
