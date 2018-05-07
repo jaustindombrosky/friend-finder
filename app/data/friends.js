9bccf16313042cced7e624aa498f1fc620e17973
jaustindombrosky <austindombrosky@gmail.com>
2018-05-07 11:49:46 -0600

    update

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friendsData = [
+    {
+      name: "Han Solo",
+      photo: "https://nerdist.com/wp-content/uploads/2015/11/Han-Solo-Main.jpg",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Wolverine",
+      photo:"https://vignette.wikia.nocookie.net/xmenmovies/images/7/73/Hugjackman-wolverine.jpg/revision/latest?cb=20120311070831",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Gene Grey",
+      photo:"https://vignette.wikia.nocookie.net/x-men/images/1/1c/162319_main.jpg/revision/latest?cb=20090121001811",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Wonder Woman",
+      photo:"http://www.comingsoon.net/assets/uploads/2018/01/justice_league_2017___diana_hq___v2_by_duck_of_satan-db3kq6k.jpg",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Cyclops",
+      photo:"https://i.annihil.us/u/prod/marvel//universe3zx/images/4/45/Cyclops_Head.jpg",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Rogue",
+      photo:"https://cdn3.whatculture.com/images/2017/04/e07c66b748cc2f4b-600x400.png",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Batman",
+      photo:"http://images.indianexpress.com/2015/04/batman-the-dark-knight-returns759.jpg",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+ 
+  module.exports = friendsData;
