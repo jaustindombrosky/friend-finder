14e298816cccd570adcab69a0fa46c34a8b6abbc
jaustindombrosky <austindombrosky@gmail.com>
2018-05-08 16:19:34 -0600

    update

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -1,18 +1,11 @@
 console.log('HTML Connected');
-
-// Node
 var path = require('path');
-
-// Both Routes
 function htmlRoutes(app) {
-  // GET ROUTE
   app.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
-  // USE ROUTE
   app.use(function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
 }
-// EXPORT to server.js
 module.exports = htmlRoutes;
--- server.js
+++ server.js
@@ -17,20 +17,14 @@ var PORT = process.env.PORT || 8080;
 
 
 
-// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+apiRoutes(app);
+htmlRoutes(app);
 
-
-// Server Routing Map 
-apiRoutes(app); // API route - Must be listed first due to the HTML default catch all "use" route
-htmlRoutes(app); // HTML route 
-
-
-// Listener - Start the server
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
